6a27eb19ddfa377e21e52ce826ccb257373b5f29
Gzim <gzimasanni@gmail.com>
2021-04-27 18:15:11 +0200

    Update Dropdown component to show in the screen

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
-const Dropdown = () => {
+const Dropdown = ({ options }) => {
+  const renderOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    )
+  })
 
   return (
     <div>
-      Comming from dropdown
+      <div className="ui form">
+        <div className="field">
+          <label className="label">Select a Color</label>
+          <div className="ui selection dropdown visible active">
+            <i className="dropdown icon"></i>
+            <div className="text">Select Color</div>
+            <div className="menu visible transition">
+              {renderOptions}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
