d562060a4d6d4b45612deda48fdd9559cdb7e547
Gzim <gzimasanni@gmail.com>
2021-04-28 17:01:29 +0200

    Add functionality to the Convert component

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,19 +2,26 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+  const [translate, setTranslate] = useState('')
+
   useEffect(() => {
-    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-      params: {
-        q: text,
-        target: language.value,
-        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-      }
-    })
+
+    const doTranslation = async () => {
+      const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: text,
+          target: language.value,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+        }
+      })
+      setTranslate(data.data.translations[0].translatedText)
+    }
+    doTranslation()
   }, [language, text])
   
   return (
     <div>
-      
+      <h1 className="ui header">{translate}</h1>
     </div>
   )
 }
